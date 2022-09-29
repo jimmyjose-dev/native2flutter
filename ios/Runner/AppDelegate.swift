362b02093cf1666f98473297c96cdb350be7b54d
Jimmy Jose <theguywhomakesapp@gmail.com>
2022-09-30 03:03:13 +0530

    iOS to Flutter communication

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -7,7 +7,34 @@ import Flutter
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+
     GeneratedPluginRegistrant.register(with: self)
+
+      let controller : FlutterViewController = window?.rootViewController as! FlutterViewController
+              
+      let deviceChannel = FlutterMethodChannel(name: "com.native2flutter", binaryMessenger:controller.binaryMessenger)
+    
+      prepareMethodHandler(channel: deviceChannel)
+      
+
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
+    
+    
+    private func prepareMethodHandler(channel: FlutterMethodChannel) {
+            
+        channel.setMethodCallHandler({
+                (call: FlutterMethodCall, result: @escaping FlutterResult) -> Void in
+
+                if call.method == "invoke_native_method" {
+
+                    channel.invokeMethod("invoke_flutter", arguments: "Hello from iOS native app")
+                }
+                else {
+                    result(FlutterMethodNotImplemented)
+                    return
+                }
+                
+            })
+        }
 }
